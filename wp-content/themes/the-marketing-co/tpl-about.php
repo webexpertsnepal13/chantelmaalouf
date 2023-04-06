ee28e6956200bb1208e90e6f104bd5db77dfe628
Wen-Sus <maharjan.susan@webexperts.com>
2023-04-06 17:55:20 +0545

    about page desktop update

--- wp-content/themes/the-marketing-co/tpl-about.php
+++ wp-content/themes/the-marketing-co/tpl-about.php
@@ -21,7 +21,7 @@ get_header();
                 <div class="col-md-5 cols">
                     <div class="content-inner-wrapper">
                         <div class="about-title">
-                            <h1>Meet Chantel Maalouf.</h1>
+                            <h3>Meet Chantel Maalouf.</h3>
                         </div>
                         <div class="about-content">
                             <p>With over 10 years’ experience catering to all clients in the makeup scene, Chantel's main passion is creating a flawless makeup look that represents you,<br>Trained specifically in beauty and bridal makeup, as well as hair styling and facial training, every woman leaves her chair feeling like their most true, beautiful self. We like to ensure a personal service with our bridal clients and an unmatched experience, which is why you will be corresponding directly with Chantel on every detail in the lead up to and on the special day.<br>Chantel believes in using only professional, high - quality cosmetic, skincare and hair products to achieve a flawless, long - lasting look that will outlast all of those happy wedding tears!</p>
@@ -31,8 +31,111 @@ get_header();
                 </div>
             </div>
         </div>
+    </div><!-- .container -->
+</section><!-- .about-section -->
+
+<section class="sip-learn-section">
+    <div class="container-fluid">
+        <div class="sip-learn-wrapper">
+            <div class="row">
+                <div class="col-md-6 col-lg-4 cols">
+                    <div class="sip-learn-content-wrap">
+                        <div class="title">
+                            <h2>Sip&Learn</h2>
+                        </div>
+                        <div class="inner">
+                            <p>Join us for a glass of Champagne (alcoholic and non-alcoholic) and the secrets revealed of two timeless makeup looks. <br>
+                            For those of you who wish to learn some quick techniques and master a certain look or just to have fun and play with makeup with a bunch of girls! <br>
+                            Join our INFORMATIVE and FUN Makeup Workshop that is tailored to educate you how to master beautiful makeovers in no time!
+                            </p>
+                            <ul><li>4hours $500 (includes special makeup gift)</li></ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-8 cols">
+                    <div class="sip-learn-thumbnail">
+                        <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2023/04/armpit.jpg" alt="sip-learn-image">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</section>
+</section><!-- .sip-learn-section -->
+<section class="section-services">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="title w-brush">
+                        <h3><span>Our services.</span></h3>
+                    </div><!-- title -->
+                </div><!-- col-md-4 -->
+                <div class="col-md-8">
+                    <div class="services-grid">
+                        <div class="service-card">
+                            <div class="card-inner">
+                                <div class="card-image">
+                                    <img src="<?php echo home_url(); ?>/wp-content/uploads/2023/04/lips.jpg" alt="">
+                                </div><!-- card-image -->
+                                <div class="button-wrap">
+                                    <a href="#" class="btn-cm btn-outline">Bridal Makeup</a>
+                                </div><!-- button-wrap -->
+                            </div><!-- card-inner -->
+                        </div><!-- service-card -->
+                        <div class="service-card">
+                            <div class="card-inner">
+                                <div class="card-image">
+                                    <img src="<?php echo home_url(); ?>/wp-content/uploads/2023/04/women-fully-makeup-scaled.jpg" alt="">
+                                </div><!-- card-image -->
+                                <div class="button-wrap">
+                                    <a href="#" class="btn-cm btn-outline">Bridal Makeup</a>
+                                </div><!-- button-wrap -->
+                            </div><!-- card-inner -->
+                        </div><!-- service-card -->
+                        <div class="service-card">
+                            <div class="card-inner">
+                                <div class="card-image">
+                                    <img src="<?php echo home_url(); ?>/wp-content/uploads/2023/04/armpit.jpg" alt="">
+                                </div><!-- card-image -->
+                                <div class="button-wrap">
+                                    <a href="#" class="btn-cm btn-outline">Bridal Makeup</a>
+                                </div><!-- button-wrap -->
+                            </div><!-- card-inner -->
+                        </div><!-- service-card -->
+                        <div class="service-card">
+                            <div class="card-inner">
+                                <div class="card-image">
+                                    <img src="<?php echo home_url(); ?>/wp-content/uploads/2023/04/girl-on-makeup.jpg" alt="">
+                                </div><!-- card-image -->
+                                <div class="button-wrap">
+                                    <a href="#" class="btn-cm btn-outline">Bridal Makeup</a>
+                                </div><!-- button-wrap -->
+                            </div><!-- card-inner -->
+                        </div><!-- service-card -->
+                        <div class="service-card">
+                            <div class="card-inner">
+                                <div class="card-image">
+                                    <img src="<?php echo home_url(); ?>/wp-content/uploads/2023/04/eye.jpg" alt="">
+                                </div><!-- card-image -->
+                                <div class="button-wrap">
+                                    <a href="#" class="btn-cm btn-outline">Bridal Makeup</a>
+                                </div><!-- button-wrap -->
+                            </div><!-- card-inner -->
+                        </div><!-- service-card -->
+                        <div class="service-card">
+                            <div class="card-inner">
+                                <div class="card-image">
+                                    <img src="<?php echo home_url(); ?>/wp-content/uploads/2023/04/sunbath.jpg" alt="">
+                                </div><!-- card-image -->
+                                <div class="button-wrap">
+                                    <a href="#" class="btn-cm btn-outline">Bridal Makeup</a>
+                                </div><!-- button-wrap -->
+                            </div><!-- card-inner -->
+                        </div><!-- service-card -->
+                    </div><!-- services-grid -->
+                </div><!-- col-md-8 -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- section-services -->
 
 <?php
 get_footer();
