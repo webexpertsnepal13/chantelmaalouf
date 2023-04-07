1ac8007b9b9ffe8163fe79c04aeae78673cb5c19
Suraj Luitel <luitel.suraj@webexpertsnepal.com>
2023-04-07 18:20:59 +0545

    updated product detail + cart page design

--- wp-content/themes/the-marketing-co/page.php
+++ wp-content/themes/the-marketing-co/page.php
@@ -17,22 +17,26 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<section class="section-default">
+			<div class="container">
+				<?php
+				while ( have_posts() ) :
+					the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+					get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-		endwhile; // End of the loop.
-		?>
+				endwhile; // End of the loop.
+				?>
+			</div><!-- container -->
+		</section><!-- section-default -->
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
