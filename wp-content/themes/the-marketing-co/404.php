0ecfc97f31c29490812b4adaa3dd4f80fd95bb92
Wen-Sus <maharjan.susan@webexperts.com>
2023-04-10 16:17:02 +0545

    faq and default page update

--- wp-content/themes/the-marketing-co/404.php
+++ wp-content/themes/the-marketing-co/404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying 404 pages (not found)
  *
@@ -10,51 +11,27 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'the-marketing-co' ); ?></h1>
-			</header><!-- .page-header -->
+	<section class="error-404 not-found">
+		<header class="page-header">
+			<div class="container">
+			<h1 class="page-title"><?php esc_html_e('Not Found', 'the-marketing-co'); ?></h1>
+			</div><!-- .container -->
+		</header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'the-marketing-co' ); ?></p>
+		<div class="page-content">
+			<div class="container">
+				<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'the-marketing-co'); ?></p>
+				<a href="" class="btn-cm btn-outline">Learn more</a>
+				<?php
+				//get_search_form();
+				?>
+			</div><!-- .container -->
+		</div><!-- .page-content -->
+	</section><!-- .error-404 -->
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'the-marketing-co' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$the_marketing_co_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'the-marketing-co' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$the_marketing_co_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_footer();
